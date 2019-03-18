33a2679590167574cf8008baa794f4127fe99821
Nick Zed <nick.z@Nicks-MacBook-Pro.local>
2019-03-18 22:29:12 +0000

    add about me section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { HeadlinePanelComponent } from './headline-panel/headline-panel.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AppComponent } from "./app.component";
+import { HeadlinePanelComponent } from "./headline-panel/headline-panel.component";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatButtonModule } from "@angular/material/button";
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeadlinePanelComponent,
-  ],
-  imports: [
-    BrowserModule,
-    FontAwesomeModule
-  ],
+  declarations: [AppComponent, HeadlinePanelComponent, AboutMeComponent],
+  imports: [BrowserModule, FontAwesomeModule, BrowserAnimationsModule, MatButtonModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
